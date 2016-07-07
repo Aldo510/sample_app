e8792c7e835900ba39b3567e34212c5b84e03757
codeamac12 <codeamac12@gmail.com>
2016-07-07 17:38:13 -0500

    Form login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
+  #Cada metodo de aqui es para la vista con el mismo
+  #nombre de esa
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)  
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+    
 end
